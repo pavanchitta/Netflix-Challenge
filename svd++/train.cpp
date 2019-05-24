68b2fdc331415a4bd2bf353af0281cd851aae36c
boubezariali <boubezari.ali@caltech.edu>
2019-05-23 20:11:26 -0700

    minor changes

--- svd++/train.cpp
+++ svd++/train.cpp
@@ -17,17 +17,21 @@ void writeToFile(string filename, vector<double> preds) {
 
 int main() {
 
-    string train_filename = "/Users/pavanchitta/CS156b-Netflix/data/um/train_probe.dta";
-    string test_filename = "/Users/pavanchitta/CS156b-Netflix/data/um/qual.dta";
-    string valid_filename = "/Users/pavanchitta/CS156b-Netflix/data/um/probe.dta";
+    string train_filename = "/Users/aliboubezari/Desktop/CS/CS156b-Netflix/data/um/train_probe.dta";
+    string test_filename =  "/Users/aliboubezari/Desktop/CS/CS156b-Netflix/data/um/qual.dta";
+    string valid_filename = "/Users/aliboubezari/Desktop/CS/CS156b-Netflix/data/um/probe.dta";
     int K = 100;
     double eta = 0.01;
     double reg = 0.02;
     Model m(458293, 17770, K, eta, reg, train_filename, test_filename, valid_filename);
     cout << "Starting training with " << K << " factors" << endl;
     m.train();
-    vector<double> preds = m.predict();
-    writeToFile("/Users/pavanchitta/CS156b-Netflix/svd_plustime2_test_predsfreq.txt", preds);
+
+    vector<double> preds_qual = m.predict();
+    vector<double> preds_train = m.predict_train();
+
+    writeToFile("/Users/aliboubezari/Desktop/CS/CS156b-Netflix/KNNoutputs/qual_preds.txt", preds_qual);
+    writeToFile("/Users/aliboubezari/Desktop/CS/CS156b-Netflix/KNNoutputs/train_preds.txt", preds_train);
 
     return 0;
 }
